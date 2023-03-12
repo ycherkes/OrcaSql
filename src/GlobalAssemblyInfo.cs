958546362e8ef3dd71f21e97a16b824e54e69fa4
ycherkes <ycherkes@outlook.com>
2023-03-12 11:55:22 +0100

    Include demo plugin binaries to sources v++

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Mark S. Rasmussen - improve.dk, Yevhen Cherkes - ycherkes@outlook.com")]
 [assembly: AssemblyProduct("OrcaSql.OSMS")]
-[assembly: AssemblyCopyright("Copyright © Mark S. Rasmussen 2011, Yevhen Cherkes 2020")]
+[assembly: AssemblyCopyright("Copyright © Mark S. Rasmussen 2011, Yevhen Cherkes 2020-2023")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
